7dd600d567a6903b18fd4d402068efd785bfa219
songchong <1972433980@qq.com>
2022-04-23 19:10:25 +0800

    增加了一些对命令的处理，待完善

--- sdk/apps/common/third_party_profile/jieli/trans_data_demo/translator_procotol.c
+++ sdk/apps/common/third_party_profile/jieli/trans_data_demo/translator_procotol.c
@@ -39,29 +39,6 @@ unsigned char check_crc(u8 *data, u8 len, u8 crc)
 		return 0;
 }
 
-//Parse the data received from the app
-void receive_data_form_app(u8 *data, u8 len)
-{
-
-	if (data[0] != HEARD_H || data[1] != HEARD_L || data[len - 1] != TAIL)
-		return;
-
-	if (!check_crc(&data[2], data[3] + 1, data[len - 2])) {
-		printf("crc check err\n");
-		return;
-	}
-
-	u8 cmd = data[2];
-
-	switch(cmd) {
-
-	case DEVICE_RECEIVE_HAND_SHAKE:
-		printf("receive app hand shake\n");
-		break;
-
-	}
-
-}
 
 
 void send_hand_shake_to_app(void)
@@ -79,4 +56,133 @@ void send_hand_shake_to_app(void)
 	//收发测试，自动发送收到的数据;for test
 	app_send_user_data_do(NULL, data, sizeof(data));
 }
+
+void send_dev_mac_to_app(void)
+{
+	u8 data[12];
+	u8 crc = 0;
+
+	data[0] = HEARD_H;
+	data[1] = HEARD_L;
+	data[2] = DEVICE_SEND_GET_MAC_ADDR;
+	/*len == cmd + data*/
+	data[3] = 0x07;
+	/*data*/
+	data[4] = 0x11;
+	data[5] = 0x22;
+	data[6] = 0x33;
+	data[7] = 0x44;
+	data[8] = 0x55;
+	data[9] = 0x66;
+	/*crc = cmd + len + data*/
+	for (int i = 2; i < 10; i++)
+		crc += data[i];
+
+	data[10] = crc;
+	data[11] = TAIL;
+	//收发测试，自动发送收到的数据;for test
+	app_send_user_data_do(NULL, data, sizeof(data));
+}
+
+
+//Parse the data received from the app
+void receive_data_form_app(u8 *data, u8 len)
+{
+
+	if (data[0] != HEARD_H || data[1] != HEARD_L || data[len - 1] != TAIL)
+		return;
+
+	if (!check_crc(&data[2], data[3] + 1, data[len - 2])) {
+		printf("crc check err\n");
+		return;
+	}
+
+	u8 cmd = data[2];
+
+	switch(cmd) {
+
+	case DEVICE_RECEIVE_HAND_SHAKE:
+		// TODO: del the timer
+		printf("receive app hand shake\n");
+		break;
+
+	case DEVICE_RECEIVE_GET_DEV_INFO:
+		// TODO:send dev info to app
+		printf("app get dev info\n");
+		break;
+
+	case DEVICE_RECEIVE_GET_MAC_ADDR:
+		// TODO:send dev mac to app
+		printf("app get dev mac addr\n");
+		send_dev_mac_to_app();
+		break;
+
+	case DEVICE_RECEIVE_SET_BLE_NAME:
+		// TODO:set ble name
+		printf("app set ble name\n");
+		break;
+
+	case DEVICE_RECEIVE_GET_BAT_LEVEL:
+		// TODO:get   bat level 
+		printf("app get bat level\n");
+		break;
+
+	case DEVICE_RECEIVE_SET_COUNTRY_INFO:
+		// TODO:set country info
+		printf("app set country info\n");
+		break;
+
+	case DEVICE_RECEIVE_GET_COUNTRY_INFO:
+		// TODO:get country info
+		printf("app get country info\n");
+		break;
+
+	case DEVICE_RECEIVE_SET_QUDAO_INFO:
+		// TODO:set qudao    info
+		printf("app set qudao info\n");
+		break;
+
+	case DEVICE_RECEIVE_GET_QUDAO_INFO:
+		// TODO:set qudao info
+		printf("app get qudao info\n");
+		break;
+
+	case DEVICE_RECEIVE_KEY_LEFT_LONG_DOWN:
+		printf("app ack key left long down\n");
+		break;
+
+	case DEVICE_RECEIVE_KEY_LEFT_LONG_UP:
+		printf("app ack key left long up\n");
+		break;
+
+	case DEVICE_RECEIVE_KEY_RIGHT_LONG_DOWN:
+		printf("app ack key right long down\n");
+		break;
+
+	case DEVICE_RECEIVE_KEY_RIGHT_LONG_UP:
+		printf("app ack key right long up\n");
+		break;
+
+	case DEVICE_RECEIVE_REQUEST_SEND_VOICE_DATA:
+		// TODO: start recoder and send voice data
+		printf("app ack request send voice\n");
+		break;
+
+	case DEVICE_RECEIVE_STOP_RECODER:
+		// TODO: stop recoder and stop send voice data
+		printf("app stop recoder and stop send voice\n");
+		break;
+
+	case DEVICE_RECEIVE_PLAY_PREVIOUS:
+		printf("app ack play previous\n");
+		break;
+
+	case DEVICE_RECEIVE_PLAY_NEXT:
+		printf("app ack play next\n");
+		break;
+
+	}
+
+}
+
 
